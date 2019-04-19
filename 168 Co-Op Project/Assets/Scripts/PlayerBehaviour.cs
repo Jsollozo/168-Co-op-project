13b2d50467cc60c4fc0311894e1c4627df407d45
Xavier Chua <xavierc.1998@gmail.com>
2019-04-18 23:51:47 -0700

    Got basic networking going
    People can join (max 2 people) and are randomly assigned a character
    (square or circle). They both have movement.
    
    Networking a bit weird tho so will have to look at that some more.

---
+++ 168 Co-Op Project/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerBehaviour : NetworkBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] Vector3 direction;
+
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        Move();
+    }
+
+    void Move()
+    {
+        rb.velocity = direction.normalized * moveSpeed;   
+    }
+}
---
+++ 168 Co-Op Project/Assets/Scripts/PlayerConnection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerConnection : NetworkBehaviour
+{
+
+    public GameObject playerPrefab;
+
+    private GameObject myPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
+        CmdSpawnPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Command]
+    void CmdSpawnPlayer()
+    {
+        GameObject go = Instantiate(playerPrefab);
+
+        myPlayer = go;
+
+        go.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);
+
+        NetworkServer.Spawn(go);
+    }
+}
