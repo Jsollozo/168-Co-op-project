bf18de1f0a076c7b76bf4b74fe6a89effb9b3afe
Xavier Chua <xavierc.1998@gmail.com>
2019-04-19 15:00:55 -0700

    Downgraded to 2018.3.12f1. Unet networking works.

---
+++ Co-Op/Assets/PlayerConnection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnection : NetworkBehaviour
+{
+
+    public GameObject playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        CmdSpawnPlayer();
+    }
+
+    GameObject myPlayerUnit;
+
+    [Command]
+    void CmdSpawnPlayer()
+    {
+        GameObject player = Instantiate(playerPrefab);
+
+        myPlayerUnit = player;
+
+        NetworkServer.SpawnWithClientAuthority(myPlayerUnit, connectionToClient);
+    }
+}
