4201ccf9381549d0eaab702c79301df4dfa182b0
Unknown <rkuhs@uci.edu>
2019-04-25 11:33:07 -0700

    Basic Enemy Spawner
    Forgot to push this the other day. Made an enemy spawner that will spawn enemies at a regular interval. Made enemies that follow the host player (will update to marked player when that mechanic is implemented). Enemies die to bullets. Forcefield does nothing for now, as the attack seems unfinished.

---
+++ Co-Op/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemySpawner : NetworkBehaviour
+{
+    public List<GameObject> enemies;
+    public float spawnRate;
+
+    public override void OnStartServer()
+    {
+        // begin spawning enemies upon server start
+        StartCoroutine(SpawnEnemies());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        // spawn enemies at fixed rate
+        for (;;)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        // to start with: spawn an enemy
+        GameObject spawnedEnemy = Instantiate(enemies[0], gameObject.transform.position, gameObject.transform.rotation);
+        NetworkServer.Spawn(spawnedEnemy);
+    }
+
+}
---
+++ Co-Op/Assets/Scripts/EnemyUnit.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemyUnit : NetworkBehaviour
+{
+    public float moveSpeed;
+
+    [SerializeField] GameObject target;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // TEMP: find a player upon spawning to track
+        CmdFindTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // just move toward target for now
+        CmdMoveTowardTarget();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Bullet")
+        {
+            Destroy(collision.collider.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    [Command]
+    void CmdFindTarget()
+    {
+        // TODO: this code will be replaced when the marked mechanic is implemented
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    [Command]
+    void CmdMoveTowardTarget()
+    {
+        if (target != null && isServer)
+        {
+            Vector3 direction = target.transform.position - transform.position;
+            transform.right = direction;
+            rb.velocity = direction.normalized * moveSpeed;
+        }
+    }
+}
